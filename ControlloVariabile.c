c50224452fc70a2d1ddd021550777ab0a78755cb
Hiro <alessandro.zhou248@gmail.com>
2026-03-26 16:06:55 +0100

    26/03 aggiunto file supporto

--- ControlloVariabile.c
+++ ControlloVariabile.c
@@ -1,20 +1,25 @@
-
 #include <stdio.h>
-
+#include <supporto.h>
 char[] controlloVariabile(char[] filename){
 	FILE *file = fopen(filename,"r");
-	
+	int numero;
+
 	if(file == Null) {
 		return "Erroew apertura file\n";
 	}
 
 	char riga[100];
 	while (fgets(riga, sizeof(riga),file) != Null) {
-		if(riga[0]=="#"):
+
+		char **p=split(riga,&numero);
+		if(p[0][0]=="#" || (p[0][0]=="/" && p[0][1]=="/") || ){
 			continue;
-		if(riga[0]=="/" and riga[1]=="/"):
-			continue;
-		
+		}
+		else{
+			if(strcmp(p[1],"main")==0){
+				continue;
+}
+		}
 	}
 
 }
